dd8ff5981ebff58ed97069fd1cf50c57230d48e5
rylanarguelles <rylanarguelles@gmail.com>
2020-05-13 23:08:09 +1000

    Add toggle add item dialog function

--- src/pages/Menu/index.jsx
+++ src/pages/Menu/index.jsx
@@ -1,23 +1,53 @@
 import React from 'react';
 import Grid from '@material-ui/core/Grid';
+import AddItemDialog from './components/AddItemDialog';
 import MenuItem from './components/MenuItem';
+import MenuController from '../../controllers/menu_controller';
 
 export default class Menu extends React.Component {
+    constructor(props) {
+        super(props);
+
+        this.toggleAddItemDialog = this.toggleAddItemDialog.bind(this);
+    }
+
+    toggleAddItemDialog(item) {
+        MenuController.setActiveItem(item);
+        MenuController.toggleAddItemDialog(true);
+    }
+
     render() {
         return (
             <Grid container direction='row' spacing={2}>
                 <Grid item>
-                    <MenuItem />
+                    <MenuItem
+                        toggleAddDialog={() =>
+                            this.toggleAddItemDialog(undefined)
+                        }
+                    />
                 </Grid>
                 <Grid item>
-                    <MenuItem />
+                    <MenuItem
+                        toggleAddDialog={() =>
+                            this.toggleAddItemDialog(undefined)
+                        }
+                    />
                 </Grid>
                 <Grid item>
-                    <MenuItem />
+                    <MenuItem
+                        toggleAddDialog={() =>
+                            this.toggleAddItemDialog(undefined)
+                        }
+                    />
                 </Grid>
                 <Grid item>
-                    <MenuItem />
+                    <MenuItem
+                        toggleAddDialog={() =>
+                            this.toggleAddItemDialog(undefined)
+                        }
+                    />
                 </Grid>
+                <AddItemDialog />
             </Grid>
         );
     }
