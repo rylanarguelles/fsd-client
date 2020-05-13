368b274ebb465ab2d36a10f0a34caee43ebad81c
rylanarguelles <rylanarguelles@gmail.com>
2020-05-13 21:24:57 +1000

    Create initial menu item component

---
+++ src/pages/Menu/components/MenuItem/index.jsx
@@ -0,0 +1,32 @@
+import React from 'react';
+import Button from '@material-ui/core/Button';
+import Card from '@material-ui/core/Card';
+import CardActions from '@material-ui/core/CardActions';
+import CardContent from '@material-ui/core/CardContent';
+import Typography from '@material-ui/core/Typography';
+import { withStyles } from '@material-ui/styles';
+
+const styles = (theme) => ({
+    item: {
+        width: 316,
+    },
+});
+
+class MenuItem extends React.Component {
+    render() {
+        const { item, classes } = this.props;
+        return (
+            <Card className={classes.item}>
+                <CardContent>
+                    <Typography variant='h6'>Beef Burger</Typography>
+                    <Typography color='textSecondary'>506 kJ</Typography>
+                </CardContent>
+                <CardActions>
+                    <Button color='primary'>Add to Cart</Button>
+                </CardActions>
+            </Card>
+        );
+    }
+}
+
+export default withStyles(styles)(MenuItem);
