e3e626aff89fcdb77d6c7270b63c2cbe1848633c
rylanarguelles <rylanarguelles@gmail.com>
2020-05-13 23:27:16 +1000

    Remove menu item id attribute

--- src/models/forms/add_cart_item_form.js
+++ src/models/forms/add_cart_item_form.js
@@ -1,12 +1,9 @@
 import { decorate, observable } from 'mobx';
 
 export default class AddCartItemForm {
-    menuItemId = 0;
-
     quantity = 1;
 }
 
 decorate(AddCartItemForm, {
-    menuItemId: observable,
     quantity: observable,
 });
