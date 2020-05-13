59b60a10790e54be777b0b1682829f7171655186
rylanarguelles <rylanarguelles@gmail.com>
2020-05-13 23:27:32 +1000

    Add add item to cart controller function

--- src/controllers/menu_controller.js
+++ src/controllers/menu_controller.js
@@ -4,12 +4,20 @@ export default class MenuController {
     static toggleAddItemDialog(shouldShow) {
         MenuStore.addCartItemForm.isShowing = shouldShow;
 
-        if(!shouldShow) {
+        if (!shouldShow) {
             MenuStore.addCartItemForm.resetForm();
         }
     }
 
     static setActiveItem(item) {
         MenuStore.activeItem = item;
+    }
+
+    static addItemToCart() {
+        MenuStore.cart.push({
+            item: MenuStore.activeItem,
+            quantity: MenuStore.addCartItemForm.form.quantity,
+        });
+        console.log(MenuStore.cart);
     }
 }
