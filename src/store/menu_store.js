9c2f8fe1cc5ba802d165352bcc9529d526c3c273
rylanarguelles <rylanarguelles@gmail.com>
2020-05-14 22:42:59 +1000

    Remove orders from menu store

--- src/store/menu_store.js
+++ src/store/menu_store.js
@@ -7,7 +7,6 @@ export class MenuStore {
     menuItems = undefined;
     activeItem = undefined;
     cart = [];
-    orders = undefined;
     addCartItemForm = addCartItemFormState;
     cartState = cartState;
     checkoutState = checkoutState;
@@ -17,7 +16,6 @@ decorate(MenuStore, {
     menuItems: observable,
     activeItem: observable,
     cart: observable,
-    orders: observable,
     addCartItemForm: observable,
     cartState: observable,
     checkoutState: observable,
