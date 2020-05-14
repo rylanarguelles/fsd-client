c5d03af1de322213742290aa229a7a78d626f20c
rylanarguelles <rylanarguelles@gmail.com>
2020-05-14 15:06:25 +1000

    Add fields for checkout dialog

--- src/components/universal/CheckoutDialog/index.jsx
+++ src/components/universal/CheckoutDialog/index.jsx
@@ -1,10 +1,17 @@
 import React from 'react';
 import { inject, observer } from 'mobx-react';
+import moment from 'moment';
 import Button from '@material-ui/core/Button';
 import Dialog from '@material-ui/core/Dialog';
 import DialogActions from '@material-ui/core/DialogActions';
 import DialogContent from '@material-ui/core/DialogContent';
 import DialogTitle from '@material-ui/core/DialogTitle';
+import FormControlLabel from '@material-ui/core/FormControlLabel';
+import Grid from '@material-ui/core/Grid';
+import MenuItem from '@material-ui/core/MenuItem';
+import Radio from '@material-ui/core/Radio';
+import RadioGroup from '@material-ui/core/RadioGroup';
+import TextField from '@material-ui/core/TextField';
 import MenuController from '../../../controllers/menu_controller';
 
 class CheckoutDialog extends React.Component {
@@ -12,11 +19,20 @@ class CheckoutDialog extends React.Component {
         super(props);
 
         this.closeCheckoutDialog = this.closeCheckoutDialog.bind(this);
+        this.onChange = this.onChange.bind(this);
     }
 
     closeCheckoutDialog() {
         MenuController.toggleCheckoutDialog(false);
     }
+
+    onChange = (property) => (e) => {
+        const { MenuStore } = this.props;
+        const {
+            checkoutState: { form },
+        } = MenuStore;
+        form[property] = e.target.value;
+    };
 
     render() {
         const { MenuStore } = this.props;
@@ -26,7 +42,6 @@ class CheckoutDialog extends React.Component {
                 form: {
                     paymentMethod,
                     amountPaid,
-                    change,
                     cardNumber,
                     cardName,
                     expiryMonth,
@@ -35,15 +50,110 @@ class CheckoutDialog extends React.Component {
                 },
             },
         } = MenuStore;
+        const cash = paymentMethod === 'Cash';
+        const card = paymentMethod === 'Card';
         return (
             <Dialog
                 open={isShowing}
                 onClose={this.closeCheckoutDialog}
                 fullWidth
-                maxWidth='sm'
+                maxWidth='xs'
             >
                 <DialogTitle>Checkout</DialogTitle>
-                <DialogContent></DialogContent>
+                <DialogContent>
+                    <Grid container direction='column' spacing={3}>
+                        <Grid item>
+                            <RadioGroup
+                                value={paymentMethod}
+                                onChange={this.onChange('paymentMethod')}
+                            >
+                                <FormControlLabel
+                                    value='Cash'
+                                    control={<Radio />}
+                                    label='Cash'
+                                />
+                                <FormControlLabel
+                                    value='Card'
+                                    control={<Radio />}
+                                    label='Credit Card'
+                                />
+                            </RadioGroup>
+                        </Grid>
+                        {cash && (
+                            <Grid item>
+                                <TextField
+                                    required
+                                    fullWidth
+                                    label='Amount Paid'
+                                    value={amountPaid}
+                                    onChange={this.onChange('amountPaid')}
+                                />
+                            </Grid>
+                        )}
+                        {card && (
+                            <Grid item>
+                                <TextField
+                                    required
+                                    fullWidth
+                                    label='Card Number'
+                                    value={cardNumber}
+                                    onChange={this.onChange('cardNumber')}
+                                />
+                            </Grid>
+                        )}
+                        {card && (
+                            <Grid item>
+                                <TextField
+                                    required
+                                    fullWidth
+                                    label='Card Name'
+                                    value={cardName}
+                                    onChange={this.onChange('cardName')}
+                                />
+                            </Grid>
+                        )}
+                        {card && (
+                            <Grid item>
+                                <TextField
+                                    label='Exp. Month'
+                                    required
+                                    fullWidth
+                                    select
+                                    value={expiryMonth}
+                                    onChange={this.onChange('expiryMonth')}
+                                >
+                                    {moment.months().map((month) => (
+                                        <MenuItem key={month} value={month}>
+                                            {month}
+                                        </MenuItem>
+                                    ))}
+                                </TextField>
+                            </Grid>
+                        )}
+                        {card && (
+                            <Grid item>
+                                <TextField
+                                    required
+                                    fullWidth
+                                    label='Exp. Year'
+                                    value={expiryYear}
+                                    onChange={this.onChange('expiryYear')}
+                                />
+                            </Grid>
+                        )}
+                        {card && (
+                            <Grid item>
+                                <TextField
+                                    required
+                                    fullWidth
+                                    label='Security Number'
+                                    value={securityNumber}
+                                    onChange={this.onChange('securityNumber')}
+                                />
+                            </Grid>
+                        )}
+                    </Grid>
+                </DialogContent>
                 <DialogActions>
                     <Button
                         color='secondary'
@@ -51,7 +161,7 @@ class CheckoutDialog extends React.Component {
                     >
                         Cancel
                     </Button>
-                    <Button color='primary'>Confirm</Button>
+                    <Button color='primary'>Pay</Button>
                 </DialogActions>
             </Dialog>
         );
