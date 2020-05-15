203013f04aa959891e0d646c73a9aa45eded52ff
rylanarguelles <rylanarguelles@gmail.com>
2020-05-15 11:26:04 +1000

    Remove customer id

--- src/models/entities/order.js
+++ src/models/entities/order.js
@@ -3,15 +3,12 @@ import { decorate, observable } from 'mobx';
 export default class Order {
     id;
 
-    customerId;
-
     orderDate;
 
     orderTotal;
 
     constructor(order) {
         this.id = order.order_id;
-        this.customerId = order.customer_id;
         this.orderDate = order.order_date;
         this.orderTotal = order.order_total;
     }
@@ -19,7 +16,6 @@ export default class Order {
 
 decorate(Order, {
     id: observable,
-    customerId: observable,
     orderDate: observable,
     orderTotal: observable,
 });
