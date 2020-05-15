28acf18c724745e47575217ace221a9502d73fc1
rylanarguelles <rylanarguelles@gmail.com>
2020-05-15 17:30:03 +1000

    Create booking store

---
+++ src/store/booking_store.js
@@ -0,0 +1,22 @@
+import { decorate, observable } from 'mobx';
+import addBookingFormState from './booking/add_booking_form';
+import bookerDetailsFormState from './booking/booker_details_form';
+
+export class BookingStore {
+    bookings = undefined;
+
+    customerBookings = [];
+
+    addBookingForm = addBookingFormState;
+
+    bookerDetailsForm = bookerDetailsFormState;
+}
+
+decorate(BookingStore, {
+    bookings: observable,
+    customerBookings: observable,
+    addBookingForm: observable,
+    bookerDetailsForm: observable,
+});
+
+export default new BookingStore();
