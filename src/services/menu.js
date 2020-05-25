9a0ccac0641e9c4eead43d7c9f6b68cd75d663cd
rylanarguelles <rylanarguelles@gmail.com>
2020-05-25 20:23:32 +1000

    Create menu service and add fetch all menu items function

---
+++ src/services/menu.js
@@ -0,0 +1,10 @@
+import api from '../utils/api';
+import MenuItem from '../models/entities/menu_item';
+
+export default class MenuService {
+    static async fetchAllMenuItems() {
+        return api.get('/menu').then((response) => {
+            return response.data.map((mi) => new MenuItem(mi));
+        });
+    }
+}
