24183e3b37b29cda3d79c762ecf9cab20a09409d
rylanarguelles <rylanarguelles@gmail.com>
2020-05-26 15:57:13 +1000

    Add component on mount function

--- src/pages/Booking/index.jsx
+++ src/pages/Booking/index.jsx
@@ -3,8 +3,13 @@ import Grid from '@material-ui/core/Grid';
 import AddBookingForm from './components/AddBookingForm';
 import BookerDetailsForm from './components/BookerDetailsForm';
 import BookingDetails from './components/BookingDetails';
+import BookingController from '../../controllers/booking_controller';
 
 export default class Booking extends React.Component {
+    componentDidMount() {
+        BookingController.getAllBookings();
+    }
+
     render() {
         return (
             <Grid container direction='row' spacing={4}>
