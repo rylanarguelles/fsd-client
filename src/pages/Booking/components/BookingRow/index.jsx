f70ed9bb118d2e7e71b3fe05903928fab7dd6e2a
rylanarguelles <rylanarguelles@gmail.com>
2020-05-27 00:22:40 +1000

    Add update and delete booking functions

--- src/pages/Booking/components/BookingRow/index.jsx
+++ src/pages/Booking/components/BookingRow/index.jsx
@@ -4,8 +4,28 @@ import Button from '@material-ui/core/Button';
 import Grid from '@material-ui/core/Grid';
 import TableCell from '@material-ui/core/TableCell';
 import TableRow from '@material-ui/core/TableRow';
+import BookingController from '../../../../controllers/booking_controller';
 
 export default class BookingRow extends React.Component {
+    constructor(props) {
+        super(props);
+
+        this.openUpdateBookingDialog = this.openUpdateBookingDialog.bind(this);
+        this.deleteBooking = this.deleteBooking.bind(this);
+    }
+
+    openUpdateBookingDialog(booking) {
+        BookingController.setActiveBooking(booking.id);
+        BookingController.toggleUpdateBookingDialog(true);
+        BookingController.fillUpdateBookingForm(booking);
+    }
+
+    deleteBooking(booking) {
+        BookingController.deleteBooking(booking);
+        BookingController.getAllBookings();
+        BookingController.searchBookings();
+    }
+
     render() {
         const { booking } = this.props;
         return (
@@ -23,12 +43,22 @@ export default class BookingRow extends React.Component {
                     >
                         <Grid item container direction='row' spacing={2}>
                             <Grid item>
-                                <Button color='primary' variant='contained'>
+                                <Button
+                                    color='primary'
+                                    variant='contained'
+                                    onClick={() =>
+                                        this.openUpdateBookingDialog(booking)
+                                    }
+                                >
                                     Change
                                 </Button>
                             </Grid>
                             <Grid item>
-                                <Button color='secondary' variant='contained'>
+                                <Button
+                                    color='secondary'
+                                    variant='contained'
+                                    onClick={() => this.deleteBooking(booking)}
+                                >
                                     Cancel
                                 </Button>
                             </Grid>
