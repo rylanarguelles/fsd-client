d0e9804b3c69dcb4465d40d5f0bccd0c5233e32b
rylanarguelles <rylanarguelles@gmail.com>
2020-05-26 15:56:33 +1000

    Add get bookings and add booking controller functions

--- src/controllers/booking_controller.js
+++ src/controllers/booking_controller.js
@@ -1,6 +1,14 @@
+import moment from 'moment';
+import BookingService from '../services/booking';
 import BookingStore from '../store/booking_store';
 
 export default class BookingController {
+    static getAllBookings() {
+        BookingService.fetchAllBookings().then((b) => {
+            BookingStore.bookings = b;
+        });
+    }
+
     static searchBookings() {
         BookingStore.customerBookings = [];
         const bookings = BookingStore.bookings;
@@ -12,7 +20,7 @@ export default class BookingController {
             Array.from(bookings).forEach((b) => {
                 if (
                     b.email === bookingEmail &&
-                    b.mobileNumber === bookingMobileNumber
+                    `${b.mobileNumber}` === bookingMobileNumber
                 ) {
                     BookingStore.customerBookings.push(b);
                 }
@@ -26,5 +34,31 @@ export default class BookingController {
         if (!shouldShow) {
             BookingStore.addBookingForm.resetForm();
         }
+    }
+
+    static addBooking() {
+        const form = BookingStore.addBookingForm.form;
+        const email = form.email;
+        const mobileNumber = form.mobileNumber;
+        const quantity = form.quantity;
+        let bookingMinute = form.bookingMinute;
+        if (bookingMinute === 0) {
+            bookingMinute = '00';
+        }
+        const bookingTime = `${form.bookingHour}: ${bookingMinute}`;
+        const bookingDate = moment();
+        bookingDate
+            .set('year', form.bookingYear)
+            .set('month', form.bookingMonth)
+            .set('day', form.bookingDay);
+
+        BookingService.addBooking({
+            bookingDate,
+            bookingTime,
+            email,
+            mobileNumber,
+            quantity,
+        });
+        this.getAllBookings();
     }
 }
