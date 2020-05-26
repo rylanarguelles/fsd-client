9d5a1dd3ec5d5bb638e9f99b23932de6d82f1449
rylanarguelles <rylanarguelles@gmail.com>
2020-05-27 00:23:14 +1000

    Add booking functionalities

--- src/controllers/booking_controller.js
+++ src/controllers/booking_controller.js
@@ -11,6 +11,7 @@ export default class BookingController {
 
     static searchBookings() {
         BookingStore.customerBookings = [];
+        this.getAllBookings();
         const bookings = BookingStore.bookings;
         const bookingEmail = BookingStore.bookerDetailsForm.form.email;
         const bookingMobileNumber =
@@ -45,12 +46,12 @@ export default class BookingController {
         if (bookingMinute === 0) {
             bookingMinute = '00';
         }
-        const bookingTime = `${form.bookingHour}: ${bookingMinute}`;
+        const bookingTime = `${form.bookingHour}:${bookingMinute}`;
         const bookingDate = moment();
         bookingDate
             .set('year', form.bookingYear)
             .set('month', form.bookingMonth)
-            .set('day', form.bookingDay);
+            .set('date', form.bookingDay);
 
         BookingService.addBooking({
             bookingDate,
@@ -58,7 +59,80 @@ export default class BookingController {
             email,
             mobileNumber,
             quantity,
+        }).then((b) => {
+            BookingStore.bookings = b;
         });
-        this.getAllBookings();
+    }
+
+    static setActiveBooking(bookingId) {
+        BookingStore.activeBooking = bookingId;
+    }
+
+    static toggleUpdateBookingDialog(shouldShow) {
+        BookingStore.updateBookingForm.isShowing = shouldShow;
+
+        if (!shouldShow) {
+            BookingStore.updateBookingForm.resetForm();
+        }
+    }
+
+    static fillUpdateBookingForm(booking) {
+        BookingStore.updateBookingForm.form.bookingDay = moment(
+            booking.date,
+        ).get('date');
+        BookingStore.updateBookingForm.form.bookingMonth = moment(
+            booking.date,
+        ).get('month');
+        BookingStore.updateBookingForm.form.bookingYear = moment(
+            booking.date,
+        ).get('year');
+
+        const bookingTime = booking.time.split(':');
+        BookingStore.updateBookingForm.form.bookingHour = parseInt(
+            bookingTime[0],
+        );
+        BookingStore.updateBookingForm.form.bookingMinute = parseInt(
+            bookingTime[1],
+        );
+    }
+
+    static updateBooking() {
+        const bookingId = BookingStore.activeBooking;
+        const form = BookingStore.updateBookingForm.form;
+        const quantity = form.quantity;
+        let bookingMinute = form.bookingMinute;
+        if (bookingMinute === 0) {
+            bookingMinute = '00';
+        }
+        const bookingTime = `${form.bookingHour}:${bookingMinute}`;
+        const bookingDate = moment();
+        bookingDate
+            .set('year', form.bookingYear)
+            .set('month', form.bookingMonth)
+            .set('date', form.bookingDay);
+
+        BookingService.updateBooking({
+            bookingId,
+            bookingDate,
+            bookingTime,
+            quantity,
+        }).then((b) => {
+            BookingStore.bookings = b;
+        });
+    }
+
+    static deleteBooking(booking) {
+        const bookingId = booking.id;
+        BookingService.deleteBooking({ bookingId }).then((b) => {
+            // nothing
+        });
+        let index = 0;
+        BookingStore.customerBookings.forEach((b) => {
+            if (b.id === bookingId) {
+                index = BookingStore.customerBookings.indexOf(b);
+            }
+        });
+        BookingStore.customerBookings.splice(index, 1);
+        this.searchBookings();
     }
 }
