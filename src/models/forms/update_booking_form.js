687d448ca9a39559c9d4b9b1784055e2df0b8b0b
rylanarguelles <rylanarguelles@gmail.com>
2020-05-27 00:20:23 +1000

    Create update booking form

---
+++ src/models/forms/update_booking_form.js
@@ -0,0 +1,24 @@
+import { decorate, observable } from 'mobx';
+import moment from 'moment';
+
+export default class UpdateBookingForm {
+    bookingHour = 8;
+
+    bookingMinute = 0;
+
+    bookingMonth = moment().get('month');
+
+    bookingDay = moment().format('DD');
+
+    bookingYear = moment().year();
+
+    quantity = 1;
+}
+
+decorate(UpdateBookingForm, {
+    bookingHour: observable,
+    bookingMinute: observable,
+    bookingMonth: observable,
+    bookingDay: observable,
+    quantity: observable,
+});
